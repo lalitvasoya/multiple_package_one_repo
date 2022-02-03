e377559fcc425a816ffd68a1388d5b70bed6017e
lalitvasoya <lalit.vasoya@tntra.io>
2022-02-03 12:07:41 +0530

    init

---
+++ log_module/setup.py
@@ -0,0 +1,24 @@
+import imp
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='log_module',  
+    version='0.1',
+    author="Lalit Vasoya",
+    author_email="lalit.vasoya@tntra.io",
+    description="Simple package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/javatechy/dokr",
+    packages=find_packages(where='log_module'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
+ 
